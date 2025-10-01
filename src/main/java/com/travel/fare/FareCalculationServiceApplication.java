87a230da2c5449336c8b043cf3347345dc34a788
RY <codes@y-unit.com>
2025-09-30 19:15:05 -0700

    Initial commit

---
+++ src/main/java/com/travel/fare/FareCalculationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.travel.fare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FareCalculationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FareCalculationServiceApplication.class, args);
+	}
+
+}
